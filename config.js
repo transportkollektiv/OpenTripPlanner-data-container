d592b716ca2696de241bc7bcc68fc54698ce1e8a
Maximilian Richt <maxi@richt.name>
2019-03-24 09:52:03 +0100

    hb: add rules to add missing stop coordinates

--- config.js
+++ config.js
@@ -10,9 +10,9 @@ const src = (id, url, fit, rules) => ({ id, url, fit, rules })
 const HB_CONFIG = {
   'id': 'hb',
   'src': [
-    src('naldo', 'http://s3.v22019038053785283.megasrv.de/hb-gtfs/gtfs-naldo_2019-08-03-05--2018-08-06.zip', false),
-    src('VGC', 'http://s3.v22019038053785283.megasrv.de/hb-gtfs/gtfs-vgc-calw-2019-03-07--2019-06-08.zip', false),
-    src('vgf', 'http://s3.v22019038053785283.megasrv.de/hb-gtfs/vgf-gtfs_2019-03-09--2019-06-08.zip', false)
+    src('naldo', 'http://s3.v22019038053785283.megasrv.de/hb-gtfs/gtfs-naldo_2019-08-03-05--2018-08-06.zip', false, ['router-hb/grfs-rules/naldo.rule']),
+    src('VGC', 'http://s3.v22019038053785283.megasrv.de/hb-gtfs/gtfs-vgc-calw-2019-03-07--2019-06-08.zip', false, ['router-hb/grfs-rules/vgc.rule']),
+    src('vgf', 'http://s3.v22019038053785283.megasrv.de/hb-gtfs/vgf-gtfs_2019-03-09--2019-06-08.zip', false, ['router-hb/grfs-rules/vgf.rule'])
   ],
   'osm': 'hb'
 }
