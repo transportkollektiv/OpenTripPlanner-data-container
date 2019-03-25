68c2166aa36c8df5ae83900f8ee267abe870a5f7
Maximilian Richt <maxi@richt.name>
2019-03-25 13:36:11 +0100

    hb: herrenberg is in the stuttgart dump from geofabrik, not tuebingen

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const HB_CONFIG = {
   'id': 'hb',
   'src': [
     src('naldo', 'http://s3.v22019038053785283.megasrv.de/hb-gtfs/gtfs-naldo_2019-08-03-05--2018-08-06.stopsnull.zip', false, ['router-hb/gtfs-rules/naldo.rule']),
-    src('VGC', 'http://s3.v22019038053785283.megasrv.de/hb-gtfs/gtfs-vgc-calw-2019-03-07--2019-06-08.stopsnull.zip', false, ['router-hb/gtfs-rules/vgc.rule']),
+    src('vgc', 'http://s3.v22019038053785283.megasrv.de/hb-gtfs/gtfs-vgc-calw-2019-03-07--2019-06-08.stopsnull.zip', false, ['router-hb/gtfs-rules/vgc.rule']),
     src('vgf', 'http://s3.v22019038053785283.megasrv.de/hb-gtfs/vgf-gtfs_2019-03-09--2019-06-08.stopsnull.zip', false, ['router-hb/gtfs-rules/vgf.rule'])
   ],
   'osm': 'hb'
@@ -137,7 +137,7 @@ const configMap = ALL_CONFIGS.map(cfg => cfg.src)
 const osm = [
   { id: 'finland', url: 'http://dev.hsl.fi/osm.finland/finland.osm.pbf' },
   { id: 'hsl', url: 'http://dev.hsl.fi/osm.hsl/hsl.osm.pbf' },
-  { id: 'hb', url: 'https://download.geofabrik.de/europe/germany/baden-wuerttemberg/tuebingen-regbez-latest.osm.pbf' }
+  { id: 'hb', url: 'https://download.geofabrik.de/europe/germany/baden-wuerttemberg/stuttgart-regbez-latest.osm.pbf' }
 ]
 
 const dem = [
