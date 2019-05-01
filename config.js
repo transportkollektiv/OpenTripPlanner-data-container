5d0dc844db2b3b719463ce71b8ed4d3e02ad671f
robbi5 <maxi@richt.name>
2019-05-01 22:18:56 +0200

    hb: use gtfs with shapes
    manually enhanced with pfaedle:
    * applied rules from gtfs-rules
    * replaced route_type 715 with 3
    * ran pfaedle with baden-wuerttemberg-latest.osm.xml

--- config.js
+++ config.js
@@ -10,9 +10,9 @@ const src = (id, url, fit, rules) => ({ id, url, fit, rules })
 const HB_CONFIG = {
   'id': 'hb',
   'src': [
-    src('naldo', 'http://s3.v22019038053785283.megasrv.de/hb-gtfs/gtfs-naldo_2019-08-03-05--2018-08-06.stopsnull.zip', false, ['router-hb/gtfs-rules/naldo.rule']),
-    src('vgc', 'http://s3.v22019038053785283.megasrv.de/hb-gtfs/gtfs-vgc-calw-2019-03-07--2019-06-08.stopsnull.zip', false, ['router-hb/gtfs-rules/vgc.rule']),
-    src('vgf', 'http://s3.v22019038053785283.megasrv.de/hb-gtfs/vgf-gtfs_2019-03-09--2019-06-08.stopsnull.zip', false, ['router-hb/gtfs-rules/vgf.rule'])
+    src('naldo', 'http://s3.v22019038053785283.megasrv.de/hb-gtfs/gtfs-naldo_2019-08-03-05--2018-08-06.shapes.zip', false, ['router-hb/gtfs-rules/naldo.rule']),
+    src('vgc', 'http://s3.v22019038053785283.megasrv.de/hb-gtfs/gtfs-vgc-calw-2019-03-07--2019-06-08.shapes.zip', false, ['router-hb/gtfs-rules/vgc.rule']),
+    src('vgf', 'http://s3.v22019038053785283.megasrv.de/hb-gtfs/vgf-gtfs_2019-03-09--2019-06-08.shapes.zip', false, ['router-hb/gtfs-rules/vgf.rule'])
   ],
   'osm': 'hb'
 }
